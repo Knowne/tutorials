c875de93c7a768be1459f1b3754a9bae35b6435d
knowne <knowne@gmail.com>
2015-08-06 10:03:31 +0100

    Created tutorial spring webservice

--- spring/tutorial.spring.webservice.wsdl.generator/src/main/java/tutorial/spring/wsdl/generator/domain/Client.java
+++ spring/tutorial.spring.webservice.wsdl.generator/src/main/java/tutorial/spring/wsdl/generator/domain/Client.java
@@ -2,6 +2,10 @@ package tutorial.spring.wsdl.generator.domain;
 
 import java.io.Serializable;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Client implements Serializable {
 
 	private static final long serialVersionUID = 1L;
